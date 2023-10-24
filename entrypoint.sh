27cc1f8d5485238486279cce81605585f469058e
José Nuñez <86788881+JosMeeh@users.noreply.github.com>
2023-10-24 08:11:06 -0400

    fixentrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Wait for the PostgreSQL server to start
-until pg_isready -h postgres-db -U postgres; do
-  echo "Waiting for the database to be ready..."
-  sleep 1
-done
+#until pg_isready -h postgres-db -U postgres; do
+ # echo "Waiting for the database to be ready..."
+  #sleep 1
+#done
 
 # Apply any necessary database migrations using TypeORM
-npx typeorm migration:run
+#npx typeorm migration:run
 
 # Start your Nest.js application
 npm start
