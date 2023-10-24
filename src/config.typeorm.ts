c99691dab1488c4e47d53f6450335c14b0fc58ad
Eduardo Consalvo <86416036+Educon001@users.noreply.github.com>
2023-10-24 09:03:56 -0400

    Update config.typeorm.ts

--- src/config.typeorm.ts
+++ src/config.typeorm.ts
@@ -1,1 +1,19 @@
+import { registerAs } from "@nestjs/config";
+import { DataSource, DataSourceOptions } from "typeorm";
+import * as process from 'process';
 
+const config = {
+    type: 'postgres',
+    host: `${process.env.DB_HOST}`,
+    port: 5432,
+    username: `${process.env.DB_USER}`,
+    password: `${process.env.DB_PASSWORD}`,
+    database: `${process.env.DB_NAME}`,
+    entities: ["./*.entity{.ts,.js}"],
+    migrations: ["./migrations/*{.ts,.js}"],
+    autoLoadEntities: true,
+    synchronize: false,
+}
+
+export default registerAs('typeorm', () => config)
+export const connectionSource = new DataSource(config as DataSourceOptions);
