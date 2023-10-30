966b268370d80740b1363740eababf4927e31ce3
Eduardo Consalvo <86416036+Educon001@users.noreply.github.com>
2023-10-30 14:04:17 -0400

    Remove env variables

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,7 +1,6 @@
 import { registerAs } from '@nestjs/config';
 import { config as dotenvConfig } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
-import process from 'process';
 import { Directory } from '../directory.entity';
 import { InitialMigration1698686400270 } from '../migrations/1698686400270-InitialMigration';
 
@@ -9,11 +8,11 @@ dotenvConfig({ path: '.env' });
 
 const config = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  host: 'localhost',
   port: 5432,
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '12345678',
-  database: process.env.DB_NAME || 'practica2',
+  username: 'postgres',
+  password: '12345678',
+  database: 'practica2',
   entities: [Directory],
   migrations: [InitialMigration1698686400270],
   autoLoadEntities: true,
