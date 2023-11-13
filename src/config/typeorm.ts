1d65f470593b2618622db58ee56c898d355d14cc
Eduardo Consalvo <86416036+Educon001@users.noreply.github.com>
2023-11-13 19:57:33 -0400

    Agregar variables de entorno

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,18 +1,15 @@
 import { registerAs } from '@nestjs/config';
-import { config as dotenvConfig } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { Directory } from '../directory.entity';
 import { InitialMigration1698686400270 } from '../migrations/1698686400270-InitialMigration';
 
-dotenvConfig({ path: '.env' });
-
 const config = {
   type: 'postgres',
-  host: 'postgres-db', /*'localhost',*/
+  host: process.env.DB_HOST || 'postgres-db',
   port: 5432,
-  username: 'postgres',
-  password: '12345678',
-  database: 'practica2',
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || '12345678',
+  database: process.env.DB_NAME || 'practica2',
   entities: [Directory],
   migrations: [InitialMigration1698686400270],
   autoLoadEntities: true,
